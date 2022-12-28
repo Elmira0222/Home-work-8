03ad373cf4e81bb5c54896517f6a0f060e176256
Elmira <zhan0222@mail.ru>
2022-12-28 17:09:10 +0600

    add tasks

---
+++ task56/Program.cs
@@ -0,0 +1,85 @@
+﻿/* Задайте прямоугольный двумерный массив. Напишите программу, которая будет 
+находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки 
+с наименьшей суммой элементов: 1 строка */
+
+// ввод числа
+int GetNumber(string text)
+{
+    int result = 0;
+    Console.WriteLine(text);
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out result)) break;
+    }
+    return result;
+}
+
+// рандомный двумерный массив
+int[,] GetArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(0, 10);
+        }
+    }
+    return array;
+}
+
+// печать массива
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Specify Rectangular Two-Dimensional Array.");
+int m = GetNumber("enter the number of lines: ");
+int n = GetNumber("enter the number of columns: ");
+if (m == n)
+{
+    Console.WriteLine("Input Error: Specify rectangular 2D array.");
+}
+else
+{
+    int[,] array = GetArray(m, n);
+    Console.WriteLine("Source array:");
+    PrintMatrix(array);
+    /*Прога находит строку с наименьшей суммой элементов
+    (считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой)
+    */
+    int minsumm = Int32.MaxValue; // Int32.MaxValue - наибольшее возможное значение типа Int32. является константой
+    int row = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int summ = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            summ += array[i, j];
+        }
+        Console.WriteLine($"Sum of row elements {i}: {summ}    ");
+        if (summ < minsumm)
+        {
+            minsumm = summ;
+            row=i;
+        }
+    }
+    Console.WriteLine($"The row with the smallest sum of elements: {row}, where is the sum of the elements = {minsumm}");
+}
+
+
---
+++ task58/Program.cs
@@ -0,0 +1,96 @@
+﻿/* Задайте две матрицы. Напишите программу, которая будет находить 
+произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18  */
+
+// ввод числа
+int GetNumber(string text)
+{
+    int result = 0;
+    Console.WriteLine(text);
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out result)) break;
+    }
+    return result;
+}
+
+// рандомный двумерный массив
+int[,] GetArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(0, 10);
+        }
+    }
+    return array;
+}
+
+// печать массива
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+/* произведение матриц
+
+Произведением двух матриц А и В называется матрица С, элемент которой, находящийся на пересечении i-й строки и j-го столбца, 
+равен сумме произведений элементов i-й строки матрицы А на соответствующие (по порядку) элементы j-го столбца матрицы В.
+Формула: C[i,j] = Sum(A[i,k]*B[k,j]) 
+Где матрица A имеет размер M х K, матрица B — K х N, и матрица C — M х N
+*/
+
+void ProductMatrix (int[,] matrixA, int[,] matrixB, int[,] matrixC)
+
+{
+  for (int i = 0; i < matrixC.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrixC.GetLength(1); j++)
+    {
+      int summ = 0;
+      for (int k = 0; k < matrixA.GetLength(1); k++)
+      {
+        summ += matrixA[i,k] * matrixB[k,j];
+      }
+      matrixC[i,j] = summ;
+    }
+  }
+}
+
+/*
+Операция умножения двух матриц выполнима только в том случае, если число столбцов в первом сомножителе равно числу строк во втором.
+В этом случае говорят, что матрицы согласованы. 
+*/
+
+Console.WriteLine("Specify Two Matrices.");
+int m = GetNumber("enter the number of rows of the first matrix: ");
+int k = GetNumber("enter the number of columns in the first matrix (equal to the number of rows in the second matrix): ");
+
+int[,] matrixA = GetArray(m, k);
+Console.WriteLine("First matrix:");
+PrintMatrix(matrixA);
+
+int n = GetNumber("enter the number of columns of the second matrix: ");
+int[,] matrixB = GetArray(k, n);
+Console.WriteLine("Second matrix:");
+PrintMatrix(matrixB);
+
+Console.WriteLine("Product of matrices:");
+int[,] matrixC = new int[m,n];
+ProductMatrix(matrixA, matrixB, matrixC);
+PrintMatrix(matrixC);
